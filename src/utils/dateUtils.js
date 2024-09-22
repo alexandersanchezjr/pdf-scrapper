0d6f4e2bbf860118132ac0b6ae3eceaece88445a
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-22 16:58:23 -0500

    change: dates names

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -7,8 +7,8 @@ const logger = require('./logger');
  */
 function getSpanishMonth(monthNumber) {
     const months = [
-        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
-        'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+        '1.Enero', '2.Febrero', '3.Marzo', '4.Abril', '5.Mayo', '6.Junio', '7.Julio',
+        '8.Agosto', '9.Septiembre', '10.Octubre', '11.Noviembre', '12.Diciembre'
     ];
     return months[monthNumber - 1];
 }
